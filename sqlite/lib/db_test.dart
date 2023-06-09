011b873814f20d72b813de22aeaddba122e715fd
georgLang9 <langgeorg61@gmail.com>
2023-06-09 11:03:39 +0200

    .

--- sqlite/lib/db_test.dart
+++ sqlite/lib/db_test.dart
@@ -1,99 +1,130 @@
 import 'dart:async';
 
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-void main() async {
-  // Avoid errors caused by flutter upgrade.
-  // Importing 'package:flutter/widgets.dart' is required.
-  WidgetsFlutterBinding.ensureInitialized();
-  final database = openDatabase(
-    join(await getDatabasesPath(), 'doggie_database.db'),
-    onCreate: (db, version) {
-      // Run the CREATE TABLE statement on the database.
-      return db.execute(
-        'CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)',
-      );
-    },
-    // Set the version. This executes the onCreate function and provides a
-    // path to perform database upgrades and downgrades.
-    version: 1,
+void main() => runApp(MyApp());
+
+int lastID = 0;
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Doggie Database',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Database database;
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController ageController = TextEditingController();
+  List<Dog> dogList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    openDatabase().then((db) {
+      database = db;
+      fetchDogs();
+    });
+  }
+
+  Future<Database> initDB() async {
+    return openDatabase(
+      join(await getDatabasesPath(), 'doggie_database.db'),
+      onCreate: (db, version) {
+        // Run the CREATE TABLE statement on the database.
+        return db.execute(
+          'CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)',
+        );
+      },
+      // Set the version. This executes the onCreate function and provides a
+      // path to perform database upgrades and downgrades.
+      version: 1,
   );
+  }
 
   Future<void> insertDog(Dog dog) async {
-    final db = await database;
-
-    await db.insert(
+    await database.insert(
       'dogs',
       dog.toMap(),
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
   }
 
-  // A method that retrieves all the dogs from the dogs table.
-  Future<List<Dog>> dogs() async {
-    final db = await database;
-
-    final List<Map<String, dynamic>> maps = await db.query('dogs');
-    return List.generate(maps.length, (i) {
-      return Dog(
-        id: maps[i]['id'],
-        name: maps[i]['name'],
-        age: maps[i]['age'],
-      );
+  Future<void> fetchDogs() async {
+    final List<Map<String, dynamic>> maps = await database.query('dogs');
+    setState(() {
+      dogList = List.generate(maps.length, (i) {
+        return Dog(
+          id: maps[i]['id'],
+          name: maps[i]['name'],
+          age: maps[i]['age'],
+        );
+      });
     });
   }
 
-  Future<void> updateDog(Dog dog) async {
-    final db = await database;
-
-    await db.update(
-      'dogs',
-      dog.toMap(),
-      where: 'id = ?',
-      whereArgs: [dog.id],
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Doggie Database'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: 'Name'),
+            ),
+            TextField(
+              controller: ageController,
+              decoration: InputDecoration(labelText: 'Age'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                final name = nameController.text;
+                final age = int.tryParse(ageController.text) ?? 0;
+                final dog = Dog(id: lastID++, name: name, age: age);
+                insertDog(dog).then((_) {
+                  fetchDogs();
+                  nameController.clear();
+                  ageController.clear();
+                });
+              },
+              child: Text('Add Dog'),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: dogList.length,
+                itemBuilder: (context, index) {
+                  final dog = dogList[index];
+                  return ListTile(
+                    title: Text(dog.name),
+                    subtitle: Text('Age: ${dog.age}'),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
-
-  Future<void> deleteDog(int id) async {
-    final db = await database;
-
-    await db.delete(
-      'dogs',
-      where: 'id = ?',
-      whereArgs: [id],
-    );
-  }
-
-  // Create a Dog and add it to the dogs table
-  var fido = const Dog(
-    id: 0,
-    name: 'Fido',
-    age: 35,
-  );
-
-  await insertDog(fido);
-
-  // Now, use the method above to retrieve all the dogs.
-  print(await dogs()); // Prints a list that include Fido.
-
-  // Update Fido's age and save it to the database.
-  fido = Dog(
-    id: fido.id,
-    name: fido.name,
-    age: fido.age + 7,
-  );
-  await updateDog(fido);
-
-  // Print the updated results.
-  print(await dogs()); // Prints Fido with age 42.
-
-  // Delete Fido from the database.
-  await deleteDog(fido.id);
-
-  // Print the list of dogs (empty).
-  print(await dogs());
 }
 
 class Dog {
